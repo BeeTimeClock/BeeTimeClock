d609826105141a18a916f8b904d3d8806cf64621
Sebastian Rakel <sebastian@devunit.eu>
2023-11-30 08:34:45 +0100

    Grouped timeslot is too eaerly to catch last day of month

--- handler/timestamp.go
+++ handler/timestamp.go
@@ -258,7 +258,7 @@ func (h *Timestamp) groupCurrentMonth(userID uint) ([]model.TimestampGroup, erro
 	currentLocation := time.Now().Location()
 
 	firstOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
-	lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
+	lastOfMonth := firstOfMonth.AddDate(0, 1, 0).Add(-1 * time.Second)
 
 	timestamps, err := h.timestamp.FindByUserIDAndDate(userID, firstOfMonth, lastOfMonth)
 	if err != nil {
