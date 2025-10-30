3904c977377a2583e04dd1457e64d7f2262b18c5
Sebastian Rakel <sebastian@devunit.eu>
2025-10-30 09:19:37 +0100

    Testing stuff

--- ui/src/pages/LoginPage.vue
+++ ui/src/pages/LoginPage.vue
@@ -1,30 +1,62 @@
 <template>
-  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+  <q-layout view="hHh Lpr fFf">
+    <!-- Be sure to play with the Layout demo on docs -->
     <q-page-container>
       <q-page class="flex flex-center bg-grey-2">
         <q-card class="q-pa-md shadow-2 my_card" bordered>
           <q-card-section class="text-center">
-            <img width="256" :src="'logo.svg'" alt="btc-logo"/>
-            <div class="text-grey-9 text-h3 text-weight-bold">Bee Time Clock</div>
-            <div class="text-grey-9 text-h5 text-weight-bold">{{ t('LABEL_SIGN_IN') }}</div>
+            <img width="256" :src="'logo.svg'" alt="btc-logo" />
+            <div class="text-grey-9 text-h3 text-weight-bold">
+              Bee Time Clock
+            </div>
+            <div class="text-grey-9 text-h5 text-weight-bold">
+              {{ t('LABEL_SIGN_IN') }}
+            </div>
           </q-card-section>
           <q-card-section>
-            <div class="text-center text-weight-thin q-mb-sm">{{ t('LABEL_LOCAL_SIGN_IN') }}</div>
-            <q-input dense outlined v-model="email" :label="t('LABEL_USERNAME')"></q-input>
-            <q-input dense outlined class="q-mt-md" v-model="password" type="password"
-                     :label="t('LABEL_PASSWORD')"></q-input>
-            <q-btn color="primary" size="md" :label="t('LABEL_SIGN_IN')" no-caps class="full-width q-mt-lg"
-                   @click="loginLocal">
+            <div class="text-center text-weight-thin q-mb-sm">
+              {{ t('LABEL_LOCAL_SIGN_IN') }}
+            </div>
+            <q-input
+              dense
+              outlined
+              v-model="email"
+              :label="t('LABEL_USERNAME')"
+            ></q-input>
+            <q-input
+              dense
+              outlined
+              class="q-mt-md"
+              v-model="password"
+              type="password"
+              :label="t('LABEL_PASSWORD')"
+            ></q-input>
+            <q-btn
+              color="primary"
+              size="md"
+              :label="t('LABEL_SIGN_IN')"
+              no-caps
+              class="full-width q-mt-lg"
+              @click="loginLocal"
+            >
             </q-btn>
           </q-card-section>
-          <q-separator/>
+          <q-separator />
           <q-card-section>
-            <div class="text-center text-weight-thin q-mb-sm">{{ t('LABEL_SSO_SIGN_IN') }}</div>
-            <q-btn v-if="authProviders?.Microsoft" color="primary" size="md" :label="t('LABEL_SIGN_IN_MICROSOFT')"
-                   no-caps class="full-width"
-                   @click="loginWithMicrosoft">
+            <div class="text-center text-weight-thin q-mb-sm">
+              {{ t('LABEL_SSO_SIGN_IN') }}
+            </div>
+            <q-btn
+              v-if="authProviders?.Microsoft"
+              color="primary"
+              size="md"
+              :label="t('LABEL_SIGN_IN_MICROSOFT')"
+              no-caps
+              class="full-width"
+              @click="loginWithMicrosoft"
+            >
               <q-avatar square class="q-ml-md" size="24px">
-                <img :src="'ms_login.png'" alt="ms-logo"/>
+                <img :src="'ms_login.png'" alt="ms-logo" />
               </q-avatar>
             </q-btn>
           </q-card-section>
@@ -35,16 +67,16 @@
 </template>
 
 <script lang="ts" setup>
-import type {AuthProviders, ErrorResponse} from 'src/models/Base';
+import type { AuthProviders, ErrorResponse } from 'src/models/Base';
 import BeeTimeClock from 'src/service/BeeTimeClock';
-import {useAuthStore} from 'stores/microsoft-auth';
-import {onMounted, ref} from 'vue';
-import {showErrorMessage} from 'src/helper/message';
-import {useRouter} from 'vue-router';
-import {msalProvider} from 'boot/microsoft-msal';
+import { useAuthStore } from 'stores/microsoft-auth';
+import { onMounted, ref } from 'vue';
+import { showErrorMessage } from 'src/helper/message';
+import { useRouter } from 'vue-router';
+import { msalProvider } from 'boot/microsoft-msal';
 import { useI18n } from 'vue-i18n';
 
-const {t} = useI18n();
+const { t } = useI18n();
 
 const router = useRouter();
 const auth = useAuthStore();
@@ -55,66 +87,77 @@ const password = ref('');
 const authProviders = ref(null as AuthProviders | null);
 
 async function loadAuthProviders() {
-  await BeeTimeClock.getAuthProviders().then((result) => {
-    if (result.status === 200) {
-      authProviders.value = result.data.Data;
-    }
-  }).catch((error: ErrorResponse) => {
-    showErrorMessage(error.response?.data.Message);
-  });
+  await BeeTimeClock.getAuthProviders()
+    .then((result) => {
+      if (result.status === 200) {
+        authProviders.value = result.data.Data;
+      }
+    })
+    .catch((error: ErrorResponse) => {
+      showErrorMessage(error.response?.data.Message);
+    });
 }
 
 function loginLocal() {
-  BeeTimeClock.login(email.value, password.value).then((result) => {
-    if (result.status === 200) {
-      auth.setAccessToken(result.data.Data.Token);
-      auth.setAuthProvider('local');
+  BeeTimeClock.login(email.value, password.value)
+    .then((result) => {
+      if (result.status === 200) {
+        auth.setAccessToken(result.data.Data.Token);
+        auth.setAuthProvider('local');
 
-      gotoDashboard();
-    }
-  }).catch((error) => {
-    showErrorMessage(error)
-    useAuthStore().logout();
-  })
+        gotoDashboard();
+      }
+    })
+    .catch((error) => {
+      showErrorMessage(error);
+      useAuthStore().logout();
+    });
 }
 
 function loginWithMicrosoft() {
   if (!msalProvider) {
-    showErrorMessage('hier ist was faul, meldet euch bei sebastian')
-    return
+    showErrorMessage('hier ist was faul, meldet euch bei sebastian');
+    return;
   }
 
-  msalProvider.msalInstance.loginPopup().then((result) => {
-    try {
+  msalProvider.msalInstance
+    .loginPopup()
+    .then((result) => {
+      try {
+        console.log('FOund ACccount directly: ', result.account)
+        console.log('Got Token: ', result.idToken);
+        auth.setAccessToken(result.idToken);
+        auth.setAuthProvider('microsoft');
 
-      auth.setAccessToken(result.idToken);
-      auth.setAuthProvider('microsoft');
+        const accounts = msalProvider.msalInstance.getAllAccounts();
+        console.log('Accounts: ', accounts)
 
-      const account = msalProvider.msalInstance.getAllAccounts()[0]
-      if (account) {
-        msalProvider.msalInstance.setActiveAccount(account)
+        if (accounts != undefined && accounts.length > 0) {
+          console.log('Set Account: ', accounts[0])
+          msalProvider.msalInstance.setActiveAccount(accounts[0]!);
+        }
+      } catch (e) {
+        let message = 'Unknown error';
+        if (e instanceof Error) {
+          message = e.message;
+        }
+        showErrorMessage(`Cant set active account: ${message}`);
+      } finally {
+        gotoDashboard();
       }
-    } catch (e) {
-      let message = 'Unknown error';
-      if (e instanceof Error) {
-        message = e.message;
-      }
-      showErrorMessage(`Cant set active account: ${message}`);
-    } finally {
-      gotoDashboard();
-    }
-  }).catch((error) => {
-    showErrorMessage('Something went wrong with microsoft popup: ' + error)
-    console.log(error)
-    auth.logout();
-  })
+    })
+    .catch((error) => {
+      showErrorMessage('Something went wrong with microsoft popup: ' + error);
+      console.log(error);
+      auth.logout();
+    });
 }
 
 function gotoDashboard() {
-  void router.push({name: 'Dashboard'})
+  void router.push({ name: 'Dashboard' });
 }
 
 onMounted(async () => {
   await loadAuthProviders();
-})
+});
 </script>
